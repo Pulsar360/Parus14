ac39d5f46075c9a0f1a7f2087575e2903f086c3e
Pulsar360 <walterfring50@gmail.com>
2022-12-08 01:25:16 +0300

    bd

---
+++ data.js
@@ -0,0 +1,303 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2-room-apart_kitchen",
+      "name": "2-room apart_kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0281937802245551,
+        "pitch": 0.011166188929855991,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6181515227232133,
+          "pitch": 0.3596507115803824,
+          "rotation": 0,
+          "target": "1-2-room-apart_living-room"
+        },
+        {
+          "yaw": -1.6424052245892575,
+          "pitch": 0.061012852720843114,
+          "rotation": 0,
+          "target": "2-2-room-apart_bedroom"
+        },
+        {
+          "yaw": -0.8083868575904756,
+          "pitch": -0.03073320681593472,
+          "rotation": 0,
+          "target": "3-3-room-apart_living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-room-apart_living-room",
+      "name": "2-room apart_living-room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.14409681157817644,
+        "pitch": 0.0693743229403907,
+        "fov": 1.2476858939469109
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.48307980712236365,
+          "pitch": 0.3258088740699616,
+          "rotation": 0,
+          "target": "0-2-room-apart_kitchen"
+        },
+        {
+          "yaw": 2.06466404121391,
+          "pitch": 0.15088497398224554,
+          "rotation": 0,
+          "target": "2-2-room-apart_bedroom"
+        },
+        {
+          "yaw": -2.5761252370503804,
+          "pitch": 0.015924122144188146,
+          "rotation": 0,
+          "target": "3-3-room-apart_living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2-room-apart_bedroom",
+      "name": "2-room apart_bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.2346747533721736,
+        "pitch": 0.06501329704452985,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6013715595509694,
+          "pitch": 0.25886137251149854,
+          "rotation": 0,
+          "target": "1-2-room-apart_living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-3-room-apart_living-room",
+      "name": "3-room apart_living-room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.9536228451951345,
+        "pitch": 0.03134103516344666,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.193106955832489,
+          "pitch": 0.0744629711508793,
+          "rotation": 0,
+          "target": "1-2-room-apart_living-room"
+        },
+        {
+          "yaw": -0.4147978995695354,
+          "pitch": 0.08498083995784356,
+          "rotation": 0,
+          "target": "6-3-room-apart_bedroom-2"
+        },
+        {
+          "yaw": 3.134700184281229,
+          "pitch": 0.14857736714063208,
+          "rotation": 0,
+          "target": "4-3-room-apart_bedroom"
+        },
+        {
+          "yaw": 2.239787153789612,
+          "pitch": 0.03061933123218097,
+          "rotation": 0,
+          "target": "5-park-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-3-room-apart_bedroom",
+      "name": "3-room apart_bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.7947960901389663,
+        "pitch": 0.10607879483365323,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.046733464040702444,
+          "pitch": 0.06912803593360195,
+          "rotation": 0,
+          "target": "3-3-room-apart_living-room"
+        },
+        {
+          "yaw": 1.0036476143963498,
+          "pitch": 0.10663959158659964,
+          "rotation": 0,
+          "target": "5-park-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-park-view",
+      "name": "Park view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.253845176241704,
+        "pitch": 0.06977868559965117,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1445512300742653,
+          "pitch": 0.11178653280335027,
+          "rotation": 0,
+          "target": "4-3-room-apart_bedroom"
+        },
+        {
+          "yaw": -0.7920721273364641,
+          "pitch": 0.13964458421488857,
+          "rotation": 0,
+          "target": "3-3-room-apart_living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-3-room-apart_bedroom-2",
+      "name": "3-room apart_bedroom-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.8317710198738109,
+        "pitch": 0.06508084053531604,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.225184509809228,
+          "pitch": 0.09491970895207658,
+          "rotation": 0,
+          "target": "3-3-room-apart_living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
